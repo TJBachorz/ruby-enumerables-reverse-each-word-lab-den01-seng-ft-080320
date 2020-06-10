be707e62fa0186be56e644bf01c40ac67892d282
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-10 00:19:23 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+
+def reverse_each_word(sentence)
+  sentence.split.collect { |word| word.reverse }.join(' ')
+end
+
+
+
+
+
+
+
+
+
+
+#def reverse_each_word(string)
+ # string_array = string.split(' ')
+  #string_array.each do |i|
+   # string_array[i].reverse
+  #end
+  #string_array.flatten
+#end
+    
+reverse_each_word("Ballons are fun!")
